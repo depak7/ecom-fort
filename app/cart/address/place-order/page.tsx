0da8bd5237e215015f850846135fbeb00244bd0b
Deepak <deepak.sd73@gmail.com>
2025-04-15 00:26:38 +0530

    fix

--- app/cart/address/place-order/page.tsx
+++ app/cart/address/place-order/page.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 import { getServerSession } from 'next-auth'
 import { authOptions } from '@/lib/auth'
-import { getCartItems, getCartItemsByStore } from '@/app/actions/cart/action'
+import { getCartItems, getCartItemsByStore, getDefaultUserAddresses } from '@/app/actions/cart/action'
 import OrderPreview from '@/components/users/cart/OrderPreview'
 
 
@@ -17,12 +17,13 @@ export default async function PlaceOrder() {
   const userId = session?.user.id;
 
   const {stores,totalPrice,totalQuantity} = await getCartItemsByStore(userId ? userId : "null");
+  const  { addresses }=await getDefaultUserAddresses(userId?userId:"");
 
 
 
   return (
     <>
-    <OrderPreview stores={stores} totalPrice={totalPrice}  totalQuantity={totalQuantity} userId={userId|| ""}/>
+    <OrderPreview stores={stores} totalPrice={totalPrice}  totalQuantity={totalQuantity} address={addresses} userId={userId|| ""}/>
     </>
     )
 }
